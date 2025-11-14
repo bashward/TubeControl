af052e8f489728c530640e35e4353eeb1024afc5
bashward <nikhilt626@gmail.com>
2025-11-14 23:00:32 +0530

    mobile responsiveness optimisation

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <title>TubeControl - Control YouTube from your phone</title>
         <meta name="description" content="TubeControl - Control YouTube from your phone" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
         <meta name="theme-color" content="#0f0f0f" />
 
         {/* Favicons */}
